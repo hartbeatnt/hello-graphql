9a84525d16b8a0cc6f379f348cf8d43557a9f308
Nathaniel Hart <Nathaniel Hart>
2017-01-25 16:01:57 -0800

    added run instructions

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const Query = new GraphQLObjectType({
   name: 'RootQueries',
   fields: () => ({
     echo: {
-      type: GraphQLStirng,
+      type: GraphQLString,
       args: {
         message: {type: GraphQLString}
       },
-      resolve(rootVale, args) {
+      resolve(rootValue, args) {
         return `received: ${args.message}`;
       }
     }
@@ -30,3 +30,16 @@ const Query = new GraphQLObjectType({
 const Schema = new GraphQLSchema({
   query: Query
 });
+
+let query = `
+  query getEcho($inputMessage: String) {
+    receivedMessage: echo(message: $inputMessage)
+  }
+`;
+
+graphql(Schema, query, null, null, {inputMessage: "Hello"}).then(function(result) {
+  console.log(result);
+});
+
+// babel-node index.js --presets "es2015"
+// in the command line to run this query
